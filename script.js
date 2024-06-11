9df5f5e6cf1f539ab0df44e2dd87a8f8c5d66ed1
obamapresidente01 <160783765+obamapresidente01@users.noreply.github.com>
2024-06-11 01:40:14 +0000

    jogo pronto

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var myGamePiece;
 var enemyGamePiece;
 var clouds = [];
 var stars = [];
+var tiros = []; // Array para armazenar os tiros
 var naveImg = new Image();
 naveImg.src = "nave.png";
 var naveInimigaImg = new Image();
@@ -12,11 +13,14 @@ var cloudImg = new Image();
 cloudImg.src = "cloud.png";
 var starImg = new Image(); // Adicionando imagem da estrela
 starImg.src = "star.png"; // Coloque o caminho correto para a imagem da estrela
+var tiroImg = new Image(); // Adicionando imagem do tiro
+tiroImg.src = "tiro.png"; // Coloque o caminho correto para a imagem do tiro
 
 var leftPressed = false;
 var rightPressed = false;
 var aPressed = false;
 var dPressed = false;
+var spacePressed = false; // Variável para controlar se a tecla de espaço está pressionada
 
 var isExplosion = false;
 var explosionFrame = 0;
@@ -27,6 +31,13 @@ var explosionRenderHeight = 64;
 var totalExplosionFrames = 6;
 var framesPerRow = 3;
 
+var backgroundMusic = new Audio("musica-acao.mp3");
+backgroundMusic.loop = true;
+backgroundMusic.volume = 0.5; // Defina o volume desejado (de 0 a 1)
+
+var tiroSound = new Audio("tiro.mp3");
+var explosionSound = new Audio("explosao.mp3");
+
 function startGame() {
     console.log("O jogo está começando!");
     myGamePiece = new component(50, 50, naveImg, 225, 900); // Nave do jogador na parte inferior
@@ -36,6 +47,11 @@ function startGame() {
     generateStars(); // Adicionando estrelas ao iniciar o jogo
     document.addEventListener("keydown", keyDownHandler, false);
     document.addEventListener("keyup", keyUpHandler, false);
+    playBackgroundMusic(); // Inicia a reprodução da música de fundo
+}
+
+function playBackgroundMusic() {
+    backgroundMusic.play();
 }
 
 var myGameArea = {
@@ -53,21 +69,32 @@ var myGameArea = {
     }
 }
 
-function component(width, height, image, x, y) {
+function component(width, height, image, x, y, isTiro = false) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
     this.image = image;
+    this.isTiro = isTiro; // Indica se é um tiro ou não
+
     this.update = function () {
         var ctx = myGameArea.context;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
+
     this.newPos = function () {
-        this.y += 1;
-        if (this.y > myGameArea.canvas.height) {
-            this.y = -this.height;
+        if (this.isTiro) { // Movimenta o tiro para cima
+            this.y -= 5; // Velocidade do tiro (altere conforme necessário)
+            if (this.y + this.height < 0) {
+                return false; // Retorna falso se o tiro sair da tela para removê-lo posteriormente
+            }
+        } else { // Movimenta a nave normal
+            this.y += 1;
+            if (this.y > myGameArea.canvas.height) {
+                this.y = -this.height;
+            }
         }
+        return true; // Retorna verdadeiro para manter o componente na tela
     }
 }
 
@@ -110,6 +137,7 @@ function updateGameArea() {
         if (checkCollision(myGamePiece, enemyGamePiece)) {
             isExplosion = true;
             explosionFrame = 0;
+            explosionSound.play(); // Reproduz o som de explosão
         }
 
         myGamePiece.update();
@@ -118,7 +146,10 @@ function updateGameArea() {
 
     // Desenha e move nuvens
     for (var i = 0; i < clouds.length; i++) {
-        clouds[i].newPos();
+        if (!clouds[i].newPos()) {
+            clouds.splice(i, 1); // Remove a nuvem se sair da tela
+            i--; // Decrementa o índice para evitar saltar uma nuvem após a remoção
+        }
         clouds[i].update();
     }
 
@@ -132,6 +163,9 @@ function updateGameArea() {
         stars[i].update();
     }
 
+    // Atualiza a posição dos tiros e remove os tiros que saem da tela
+    updateTiros();
+
     requestAnimationFrame(updateGameArea);
 }
 
@@ -144,6 +178,11 @@ function keyDownHandler(event) {
         aPressed = true;
     } else if (event.key === "d" || event.key === "D") {
         dPressed = true;
+    } else if (event.key === " ") { // Se a tecla pressionada for a barra de espaço
+        if (!spacePressed) { // Verifica se a tecla de espaço não estava pressionada antes (evita múltiplos tiros com uma única pressão)
+            fire(); // Chama a função para disparar um tiro
+            spacePressed = true; // Define spacePressed como verdadeiro para indicar que a tecla de espaço está sendo pressionada
+        }
     }
 }
 
@@ -156,6 +195,36 @@ function keyUpHandler(event) {
         aPressed = false;
     } else if (event.key === "d" || event.key === "D") {
         dPressed = false;
+    } else if (event.key === " ") { // Se a tecla de espaço for liberada
+        spacePressed = false; // Define spacePressed como falso para indicar que a tecla de espaço não está mais pressionada
+    }
+}
+
+function fire() {
+    var tiroX = myGamePiece.x + myGamePiece.width / 2; // Posição X inicial do tiro (centro da nave)
+    var tiroY = myGamePiece.y; // Posição Y inicial do tiro (topo da nave)
+    tiros.push(new component(5, 10, tiroImg, tiroX, tiroY, true)); // Cria um novo tiro e o adiciona ao array de tiros
+    tiroSound.play(); // Reproduz o som de tiro
+}
+
+function updateTiros() {
+    for (var i = 0; i < tiros.length; i++) {
+        var tiro = tiros[i];
+        if (!tiro.newPos()) {
+            tiros.splice(i, 1); // Remove o tiro se sair da tela
+            i--; // Decrementa o índice para evitar saltar um tiro após a remoção
+        } else {
+            // Verifica a colisão entre o tiro e a nave inimiga
+            if (checkCollision(tiro, enemyGamePiece)) {
+                isExplosion = true; // Ativa a explosão
+                explosionFrame = 0;
+                tiros.splice(i, 1); // Remove o tiro
+                i--; // Decrementa o índice para evitar problemas com o loop
+                explosionSound.play(); // Reproduz o som de explosão
+            } else {
+                tiro.update(); // Atualiza a posição do tiro
+            }
+        }
     }
 }
 
@@ -170,8 +239,8 @@ function drawExplosion() {
     var ctx = myGameArea.context;
     var sx = (explosionFrame % framesPerRow) * explosionFrameWidth;
     var sy = Math.floor(explosionFrame / framesPerRow) * explosionFrameHeight;
-    var x = ((myGamePiece.x + myGamePiece.width / 2) + (enemyGamePiece.x + enemyGamePiece.width / 2)) / 2 - explosionRenderWidth / 2;
-    var y = ((myGamePiece.y + myGamePiece.height / 2) + (enemyGamePiece.y + enemyGamePiece.height / 2)) / 2 - explosionRenderHeight / 2;
+    var x = enemyGamePiece.x + (enemyGamePiece.width - explosionRenderWidth) / 2; // Posição X da explosão na nave inimiga
+    var y = enemyGamePiece.y + (enemyGamePiece.height - explosionRenderHeight) / 2; // Posição Y da explosão na nave inimiga
 
     ctx.drawImage(explosionImg, sx, sy, explosionFrameWidth, explosionFrameHeight, x, y, explosionRenderWidth, explosionRenderHeight);
 
@@ -182,3 +251,5 @@ function drawExplosion() {
         enemyGamePiece = new component(50, 50, naveInimigaImg, 225, 0); // Reposiciona a nave inimiga
     }
 }
+
+startGame(); // Inicia o jogo quando a página carrega
